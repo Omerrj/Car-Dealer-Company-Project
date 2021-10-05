6e9609929fbe3ac68714bed272b79a53247cf39c
Omerrj <67536912+Omerrj@users.noreply.github.com>
2021-10-05 10:24:23 +0300

    add CarForSale list and some methods

--- src/classes/CarForSale.java
+++ src/classes/CarForSale.java
@@ -1,5 +1,33 @@
 package classes;
 
-public class CarForSale {
+import java.util.*;
 
+public class CarForSale {
+    List<Car> list = new LinkedList<Car>();
+
+    public CarForSale(Car car) {
+        list.add(car);
+    }
+
+    public Object getCar(String id) {
+        for (int i = 0; i < list.size(); i++) {
+            Car car = list.get(i);
+            if (car.getId() == id)
+                return car;
+
+        }
+
+        return false;
+    }
+
+    public void getList() {
+        System.out.println("List of cars for sale");
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).getInfo());
+        }
+    }
+
+    public void sellCar(String id) {
+    }
 }
