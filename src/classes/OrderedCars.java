f44fb2785a9939e4c928c3e6e93dc58c21f40459
Omerrj <67536912+Omerrj@users.noreply.github.com>
2021-10-18 19:13:38 +0300

    implement basic OrderedCars

--- src/classes/OrderedCars.java
+++ src/classes/OrderedCars.java
@@ -1,5 +1,56 @@
 package classes;
 
-public class OrderedCars {
+import java.util.*;
+
+import interfaces.ListInterface;
+
+public class OrderedCars implements ListInterface {
+    List<Car> list = new LinkedList<Car>();
+
+    public OrderedCars() {
+        System.out.println("List of Ordered Cars Created");
+    }
+
+    public OrderedCars(Car car) {
+        this();
+        list.add(car);
+    }
+
+    @Override
+    public Object getCar(String id) {
+        for (int i = 0; i < list.size(); i++) {
+            Car car = list.get(i);
+            if (car.getId() == id)
+                return car;
+
+        }
+
+        return false;
+    }
+
+    @Override
+    public void getList() {
+        System.out.println("List of Ordered Cars");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).getId());
+            System.out.println(i + 1 + "-" + list.get(i).toString());
+        }
+    }
+
+    @Override
+    public void addToList(Car car) {
+        list.add(car);
+        System.out.println("Car Ordered " + "( " + car.toString() + " )");
+
+    }
+
+    @Override
+    public void removeFromList(String id) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getId() == id)
+                list.remove(i);
+
+        }
+    }
 
 }
