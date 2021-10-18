73c24037fe84f2b6f11df83eba7d7ad71efa4a63
Omerrj <67536912+Omerrj@users.noreply.github.com>
2021-10-18 19:13:20 +0300

    implement basic CarForSale

--- src/classes/CarForSale.java
+++ src/classes/CarForSale.java
@@ -2,16 +2,24 @@ package classes;
 
 import java.util.*;
 
-public class CarForSale {
+import interfaces.ListInterface;
+
+public class CarForSale implements ListInterface {
     List<Car> list = new LinkedList<Car>();
 
+    public CarForSale() {
+        System.out.println("List of cars for sales created");
+    }
+
     public CarForSale(Car car) {
-        list.add(car);
+        this();
+        addToList(car);
     }
 
     public Object getCar(String id) {
         for (int i = 0; i < list.size(); i++) {
             Car car = list.get(i);
+
             if (car.getId() == id)
                 return car;
 
@@ -24,10 +32,26 @@ public class CarForSale {
         System.out.println("List of cars for sale");
 
         for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i).getInfo());
+            System.out.println(i + 1 + "-" + list.get(i).toString());
         }
     }
 
-    public void sellCar(String id) {
+    @Override
+    public void addToList(Car car) {
+        list.add(car);
+        System.out.println("Car added to the sale list " + "( " + car.toString() + " )");
+
+    }
+
+    @Override
+    public void removeFromList(String id) {
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getId() == id) {
+                System.out.println("Car sold " + "( " + list.get(i).toString() + " )");
+                list.remove(i);
+            }
+
+        }
     }
 }
