80751c7c6281fe649fafa83470d2461ef7cca50a
Omerrj <67536912+Omerrj@users.noreply.github.com>
2021-11-09 09:50:45 +0300

    setups socket

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import view.MainView;
 
 import java.io.*;
+import java.net.Socket;
 
 import controller.DataBaseController;
+import utils.MySocket;
 
 public class Main {
 
@@ -11,6 +13,9 @@ public class Main {
                 DataBaseController.loadDataBase();
 
                 MainView mainView = new MainView();
+                MySocket socket = new MySocket();
+
+                socket.startConnection("localhost", 3000);
                 mainView.run();
 
         }
